ed04741128f398101f4b45042be6d656c956ff5f
muditmehrotra1987 <muditmehrotra2050@yahoo.com>
2013-05-08 13:16:56 +0530

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,35 @@
+class HomeController < ApplicationController
+  
+  def index
+
+  end
+
+  def about
+    render :layout => 'simplelayout'
+  end
+
+  def contact
+    render :layout => 'simplelayout'
+  end
+  
+  def hml
+    render :layout => 'simplelayout'
+  end
+  
+  def product
+    render :layout => 'simplelayout'
+  end
+  
+  def portfolio
+    render :layout => 'simplelayout'
+  end
+  
+  def service
+    render :layout => 'simplelayout'
+  end
+  
+  def career
+    render :layout => 'simplelayout'
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Herokuwebsite::Application.config.secret_token = '4f3c9ce6537c027a8d39ad8ddd28bd68926add30a8843f1d1f06578b41008c8eeae8b1445414023eb4a194e1585cd775b2e70e60eebab55eee942bee4428f9f6'
